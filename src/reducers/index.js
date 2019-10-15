b9f4965df8fa7c23ba4433d0109d0a79cf094ca1
Florian Vallen <florian.vallen@gmail.com>
2019-10-15 18:56:49 +0200

    removed not needed reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux"
 
-import menuItems from "./menuItems"
 import documents from "./documents"
 
 const rootReducer = combineReducers({
-  menuItems,
   documents
 })
 
